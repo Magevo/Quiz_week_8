20b29d07732209d8ff6c352ba54c7c2bd7814440
Magevo <jackson.scown@icloud.com>
2023-09-16 02:12:11 +1000

    Created Python 3 code that loads the data from the climate.csv and updated it into the figure co2_temp_1.

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,39 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
 years = []
 co2 = []
 temp = []
 
-plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+def grab():
+    connection = sqlite3.connect(r".\climate.db")
+    cursor = connection.cursor()
 
-plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+    cursor.execute("SELECT  Year, CO2, Temperature FROM ClimateData")
+    data = cursor.fetchall()
+
+    for info in data:
+        years.append(info[0])
+        co2.append(info[1])
+        temp.append(info[2])
+
+    connection.close()
+
+
+def plot():
+    plt.subplot(2, 1, 1)
+    plt.plot(years, co2, 'b--') 
+    plt.title("Climate Data") 
+    plt.ylabel("[CO2]") 
+    plt.xlabel("Year (decade)") 
+
+    plt.subplot(2, 1, 2)
+    plt.plot(years, temp, 'r*-') 
+    plt.ylabel("Temp (C)") 
+    plt.xlabel("Year (decade)") 
+    plt.show() 
+    plt.savefig("co2_temp_1.png") 
+
+if __name__ == "__main__":
+    grab()
+    plot()
